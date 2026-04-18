e869ecb82f6c3d013fee4f704323eb3bdf0ff116
Ayush-Sawarn <ayushsawarn166@gmail.com>
2026-04-19 05:09:02 +0530

    Remove build and devel

--- build/turtlebot3_simulations/MoveObject/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayush/DL_Hackathon/Deimos_task1/src/turtlebot3_simulations/MoveObject
-# Build directory: /home/ayush/DL_Hackathon/Deimos_task1/build/turtlebot3_simulations/MoveObject
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
